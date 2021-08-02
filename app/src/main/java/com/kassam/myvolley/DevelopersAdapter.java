9e4e37a276f626fb1ad0e07326fa2aba174231fb
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-08-02 15:10:20 +0300

    volley implementation working - java developers showing in recycler view

--- app/src/main/java/com/kassam/myvolley/DevelopersAdapter.java
+++ app/src/main/java/com/kassam/myvolley/DevelopersAdapter.java
@@ -1,31 +1,67 @@
 package com.kassam.myvolley;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class DevelopersAdapter extends RecyclerView.Adapter<DevelopersAdapter.ViewHolder> {
+
+    private List<DeveloperList> developerList;
+    private Context mContext;
+
+
+    public DevelopersAdapter(List<DeveloperList> developerList, Context context){
+        this.developerList = developerList;
+        this.mContext = context;
+    }
+
     @NonNull
     @Override
-    public DevelopersAdapter.ViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
-        return null;
+    public DevelopersAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.developer_list, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull  DevelopersAdapter.ViewHolder holder, int position) {
+        final DeveloperList currentDeveloper = developerList.get(position);
 
+        holder.login.setText(currentDeveloper.getLogin());
+        holder.html_url.setText(currentDeveloper.getHtml_url());
+
+        Picasso.with(mContext)
+                .load(currentDeveloper.getAvatar_url())
+                .into(holder.avatar_url);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return developerList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView login;
+        public ImageView avatar_url;
+        public TextView html_url;
+        public LinearLayout linearLayout;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            login = itemView.findViewById(R.id.username);
+            avatar_url = itemView.findViewById(R.id.imageView);
+            html_url = itemView.findViewById(R.id.html_url);
+            linearLayout = itemView.findViewById(R.id.linearLayout);
         }
     }
 }
